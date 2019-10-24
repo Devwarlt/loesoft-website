0d194787fdd2dfc5fcadd711c6192d3ed612352e
Devwarlt <devwarlt@loesoft.org>
2019-10-23 23:47:51 -0200

    Include missing dependencies on PacketHandler.php

--- src/php/handlers/PacketHandler.php
+++ src/php/handlers/PacketHandler.php
@@ -6,6 +6,17 @@
  * Time: 11:20
  */
 
+include "../utilities/Utils.php";
+include "../utilities/Database.php";
+include "../utilities/DateUtils.php";
+include "../utilities/DatabaseUtils.php";
+include "IHandler.php";
+include "Debuggable.php";
+include "PacketId.php";
+include "LoginHandler.php";
+include "RegisterHandler.php";
+include "PacketManager.php";
+
 use php\handlers\PacketManager as pm;
 
 $pm = pm::getSingleton();
